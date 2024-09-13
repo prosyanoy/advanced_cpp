3f5444004174de0a6fa7972116750a8994e3bd3b
Ilia Prosianoi <prosyanoyilya@mail.ru>
2024-09-13 09:26:43 +0300

    memory/dedup

--- memory/dedup/dedup.h
+++ memory/dedup/dedup.h
@@ -6,7 +6,8 @@
 
 using std::string;
 
-std::vector<std::unique_ptr<string>> Duplicate(const std::vector<std::shared_ptr<string>>& items) {
+std::vector<std::unique_ptr<string>> Duplicate(
+    const std::vector<std::shared_ptr<string>>& items) {
     std::vector<std::unique_ptr<string>> out(items.size());
     for (size_t i = 0; i < items.size(); i++) {
         out[i] = std::make_unique<string>(*items[i]);
@@ -14,7 +15,8 @@ std::vector<std::unique_ptr<string>> Duplicate(const std::vector<std::shared_ptr
     return out;
 }
 
-std::vector<std::shared_ptr<string>> DeDuplicate(const std::vector<std::unique_ptr<string>>& items) {
+std::vector<std::shared_ptr<string>> DeDuplicate(
+    const std::vector<std::unique_ptr<string>>& items) {
     std::vector<std::shared_ptr<string>> out(items.size());
     for (size_t i = 0; i < items.size(); i++) {
         string s = *items[i];
