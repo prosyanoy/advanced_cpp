bbe1d5005fd73569164ad557771d314b4680ff24
Ilia Prosianoi <prosyanoyilya@mail.ru>
2024-09-13 09:44:12 +0300

    memory/string-view

--- memory/string-view/string_view.h
+++ memory/string-view/string_view.h
@@ -5,28 +5,28 @@
 #include <algorithm>
 
 class StringView {
-    const char* sv;
-    size_t sv_size;
+    const char* sv_;
+    size_t sv_size_;
 
 public:
     StringView(const std::string& s, size_t pos = 0, size_t size = std::string::npos) {
-        sv = s.c_str() + pos;
-        sv_size = std::min(size, s.size() - pos);
+        sv_ = s.c_str() + pos;
+        sv_size_ = std::min(size, s.size() - pos);
     }
 
-    StringView(const char* s) : sv(s) {
-        sv_size = std::strlen(s);
+    StringView(const char* s) : sv_(s) {
+        sv_size_ = std::strlen(s);
     }
 
-    StringView(const char* s, size_t size) : sv(s) {
-        sv_size = size;
+    StringView(const char* s, size_t size) : sv_(s) {
+        sv_size_ = size;
     }
 
     char operator[](size_t index) const {
-        return *(sv + index);
+        return *(sv_ + index);
     }
 
     size_t Size() const {
-        return sv_size;
+        return sv_size_;
     }
 };
