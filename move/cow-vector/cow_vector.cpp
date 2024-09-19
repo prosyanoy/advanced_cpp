60d78ec4060d29b2c8365996e62ee5e086d6b08a
Ilia Prosianoi <prosyanoyilya@mail.ru>
2024-09-19 23:19:36 +0300

    move/cow-vector

---
+++ move/cow-vector/cow_vector.cpp
@@ -0,0 +1,80 @@
+#include "cow_vector.h"
+void COWVector::CleanState() {
+    if (!state_->ref_count) {
+        delete state_;
+    } else {
+        --state_->ref_count;
+    }
+}
+COWVector::COWVector() : state_(new State{0, {}, false}) {
+}
+COWVector::~COWVector() {
+    CleanState();
+}
+
+COWVector::COWVector(const COWVector& other) {
+    state_ = other.state_;
+    ++(state_->ref_count);
+}
+COWVector& COWVector::operator=(const COWVector& other) {
+    CleanState();
+    state_ = other.state_;
+    ++(state_->ref_count);
+    return *this;
+}
+
+COWVector::COWVector(COWVector&& other) {
+    state_ = other.state_;
+    other.state_ = nullptr;
+    // ref_count stays the same
+}
+
+COWVector& COWVector::operator=(COWVector&& other) {
+    CleanState();
+    state_ = other.state_;
+    other.state_ = nullptr;
+    // ref_count stays the same
+    return *this;
+}
+
+size_t COWVector::Size() const {
+    return state_->v.size();
+}
+
+void COWVector::Resize(size_t size) {
+    state_->v.resize(size);
+}
+
+const std::string& COWVector::Get(size_t at) {
+    return state_->v[at];
+}
+
+const std::string& COWVector::Back() {
+    return state_->v.back();
+}
+
+void COWVector::PushBack(const std::string& value) {
+    if (state_->deep_copy || state_->v.empty()) {
+        state_->v.push_back(value);
+    } else {
+        State* temp = new State{*state_};
+        CleanState();
+        state_ = temp;
+        state_->deep_copy = true;
+        state_->ref_count = 1;
+        state_->v.push_back(value);
+    }
+}
+
+void COWVector::Set(size_t at, const std::string& value) {
+    if (state_->deep_copy || state_->v.empty()) {
+        state_->v[at] = value;
+    } else {
+        State* temp = new State{*state_};
+        CleanState();
+        state_ = temp;
+        state_->deep_copy = true;
+        state_->ref_count = 1;
+        state_->v[at] = value;
+    }
+}
---
+++ move/cow-vector/cow_vector.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+struct State {
+    int ref_count; // сколько векторов делят этот State между собой.
+    std::vector<std::string> v;
+    bool deep_copy;
+};
+
+class COWVector {
+public:
+    void CleanState();
+    COWVector();
+    ~COWVector();
+
+    COWVector(const COWVector& other);
+    COWVector& operator=(const COWVector& other);
+
+    COWVector(COWVector&& other);
+    COWVector& operator=(COWVector&&);
+
+    size_t Size() const;
+
+    void Resize(size_t size);
+
+    const std::string& Get(size_t at);
+    const std::string& Back();
+
+    void PushBack(const std::string& value);
+
+    void Set(size_t at, const std::string& value);
+
+private:
+    State* state_;
+};
