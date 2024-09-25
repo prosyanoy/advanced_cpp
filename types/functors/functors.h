9525d6f24375687b49363c26a4ca09fb00455818
Ilia Prosianoi <prosyanoyilya@mail.ru>
2024-09-26 00:25:29 +0300

    types/functors

---
+++ types/functors/functors.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <vector>
+
+template <class Functor>
+class ReverseBinaryFunctor {
+public:
+    explicit ReverseBinaryFunctor(Functor functor) : functor_(functor) {}
+
+    template <typename T>
+    bool operator()(const T& a, const T& b) const {
+        return functor_(b, a);
+    }
+
+private:
+    Functor functor_;
+};
+
+template <class Functor>
+class ReverseUnaryFunctor {
+public:
+    explicit ReverseUnaryFunctor(Functor functor) : functor_(functor) {}
+
+    template <typename T>
+    bool operator()(const T& a) const {
+        return !functor_(a);
+    }
+
+private:
+    Functor functor_;
+};
+
+template <class Functor>
+ReverseUnaryFunctor<Functor> MakeReverseUnaryFunctor(Functor functor) {
+    return ReverseUnaryFunctor<Functor>(functor);
+}
+
+template <class Functor>
+ReverseBinaryFunctor<Functor> MakeReverseBinaryFunctor(Functor functor) {
+    return ReverseBinaryFunctor<Functor>(functor);
+}
