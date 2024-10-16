67f848eeaadc583df6e7f45df69786085980030a
Ilia Prosianoi <prosyanoyilya@mail.ru>
2024-10-16 12:24:52 +0300

    errors/tryhard

---
+++ errors/tryhard/try.h
@@ -0,0 +1,100 @@
+#pragma once
+
+#include <exception>
+#include <stdexcept>
+
+template <class T>
+class Try {
+    T value_;
+    bool IsEmpty_;
+    std::exception_ptr ex_;
+
+public:
+    Try() : value_(), IsEmpty_(true), ex_(nullptr) {
+    }
+    Try(T value) : value_(value), IsEmpty_(false), ex_(nullptr) {
+    }
+    Try(const std::exception& ex) : value_(), IsEmpty_(true) {
+        ex_ = std::make_exception_ptr(std::runtime_error(ex.what()));
+    }
+    Try(std::exception_ptr ex, bool) : value_(), IsEmpty_(true), ex_(ex) {
+    }
+    Try(const char* ex) : value_(), IsEmpty_(true), ex_(std::make_exception_ptr(std::runtime_error(ex))) {
+    }
+    Try(int ex, bool) : value_(), IsEmpty_(true), ex_(std::make_exception_ptr(std::runtime_error(std::strerror(ex)))) {
+    }
+
+    const T& Value() {
+        if (ex_) {
+            std::rethrow_exception(ex_);
+        } else {
+            if (IsEmpty_) {
+                throw std::runtime_error("Object is empty");
+            } else {
+                return value_;
+            }
+        }
+    }
+    void Throw() {
+        if (ex_) {
+            std::rethrow_exception(ex_);
+        } else {
+            throw std::runtime_error("No exception");
+        }
+    }
+    
+    bool IsFailed() {
+        return ex_ != nullptr;
+    }
+};
+
+template <>
+class Try<void> {
+    bool IsEmpty_;
+    std::exception_ptr ex_;
+
+public:
+    Try() : IsEmpty_(true), ex_(nullptr) {
+    }
+    Try(const std::exception& ex) : IsEmpty_(true) {
+        ex_ = std::make_exception_ptr(std::runtime_error(ex.what()));
+    }
+    Try(std::exception_ptr ex, bool) : IsEmpty_(true), ex_(ex) {
+    }
+    Try(const char* ex) : IsEmpty_(true), ex_(std::make_exception_ptr(std::runtime_error(ex))) {
+    }
+    Try(int ex, bool) : IsEmpty_(true), ex_(std::make_exception_ptr(std::runtime_error(std::strerror(ex)))) {
+    }
+    void Throw() {
+        if (ex_) {
+            std::rethrow_exception(ex_);
+        } else {
+            throw std::runtime_error("No exception");
+        }
+    }
+    bool IsFailed() {
+        return ex_ != nullptr;
+    }
+};
+
+template <class Function, class... Args>
+auto TryRun(Function func, Args... args) {
+    using ReturnType = decltype(func(args...));
+    try {
+        func(args...);
+        if constexpr (std::is_void_v<ReturnType>) {
+            return Try<void>();
+        } else {
+            return Try<ReturnType>(func(args...));
+        }
+    } catch (const std::exception& ex) {
+        std::exception_ptr eptr = std::current_exception();
+        return Try<ReturnType>(eptr, true);
+    } catch (const char* ex) {
+        return Try<ReturnType>(ex);
+    } catch (int ex) {
+        return Try<ReturnType>(ex, true);
+    } catch (...) {
+        return Try<ReturnType>("Unknown exception");
+    }
+}
