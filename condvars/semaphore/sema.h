a643761900f0412a7838e4ad9281ef4b4fa7ee1a
Ilia Prosianoi <prosyanoyilya@mail.ru>
2024-11-28 14:58:49 +0300

    condvars/semaphore

---
+++ condvars/semaphore/sema.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <mutex>
+#include <condition_variable>
+
+class DefaultCallback {
+public:
+    void operator()(int& value) {
+        --value;
+    }
+};
+
+class Semaphore {
+public:
+    Semaphore(int count) : count_(count) {
+    }
+
+    void Leave() {
+        bool was_empty = false;
+        {
+            std::lock_guard guard{mutex_};
+            ++count_;
+            was_empty = (count_ == 1);
+        }
+        if (was_empty) {
+            cv_.notify_one();
+        }
+    }
+
+    template <class Func>
+    void Enter(Func callback) {
+        int order = counter1_++;
+        while (true) {
+            std::unique_lock<std::mutex> guard(mutex_);
+            cv_.wait(guard, [this] { return count_ > 0; });
+            if (counter2_ == order) {
+                callback(count_);
+                ++counter2_;
+                return;
+            }
+        }
+    }
+
+    void Enter() {
+        DefaultCallback callback;
+        Enter(callback);
+    }
+
+private:
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    int count_ = 0;
+    std::atomic<int> counter1_ = 0;
+    std::atomic<int> counter2_ = 0;
+};
